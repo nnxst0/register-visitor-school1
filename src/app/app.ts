4ea89afb4754e1414fcad56996da1128166e1264
Wilasinee2211 <antsamui123@gmail.com>
2025-12-08 10:26:55 +0700

    all ant

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './sidebar/sidebar';
+import { Main } from './main/main';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Sidebar,Main ,CommonModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('register-visitor-school');
+  isSidebarCollapsed = false;
+
+  onSidebarChange(collapsed: boolean) {
+    this.isSidebarCollapsed = collapsed;
+  }
+  
+  
 }
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule ,CommonModule, NgClass],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+   isSidebarCollapsed = input.required<boolean>();
+  changeIsSidebarCollapsed = output<boolean>();
+  items = [
+    {
+      routeLink: 'dashboard',
+      icon: 'fal fa-home',
+      label: 'ลงทะเบียน',
+    },
+    {
+      routeLink: 'products',
+      icon: 'fal fa-box-open',
+      label: 'ข้อมูลผู้ติดต่อ',
+    },
+    {
+      routeLink: 'pages',
+      icon: 'fal fa-file',
+      label: 'คืนบัตร',
+    },
+    {
+      routeLink: 'settings',
+      icon: 'fal fa-cog', 
+      label: 'ส่งออกข้อมูล',
+    },
+  ];
 
+  toggleCollapse(): void {
+    this.changeIsSidebarCollapsed.emit(!this.isSidebarCollapsed());
+  }
+
+  closeSidenav(): void {
+    this.changeIsSidebarCollapsed.emit(true);
+  }
 }
